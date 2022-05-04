048111d211c44139606c7f7c2a1594f39ae856f4
mostafa8722 <mostafa.8722@gmail.com>
2022-05-04 13:15:49 +0430

    Initial commit

---
+++ app/src/main/java/com/example/flash_light_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flash_light_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
